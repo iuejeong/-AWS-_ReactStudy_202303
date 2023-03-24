6af17126b2714565c1223bb73a3d7048a6e02907
iuejeong <dmlgus3585@naver.com>
2023-03-24 17:53:32 +0900

    렌더링

--- react/react_study/src/App.js
+++ react/react_study/src/App.js
@@ -1,5 +1,12 @@
+
 import { Route, Routes } from "react-router-dom";
 import InputSample from "./pages/InputSample/InputSample";
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import MainAside from "./components/Aside/MainAside/MainAside";
+import { Global } from "@emotion/react";
+import { reset } from "./styles/Global/reset";
+import UserList from "./pages/UserList/UserList";
+
 
 function Test1() {
   return (<h1>Test1</h1>)
@@ -9,13 +16,17 @@ function Test2() {
   return (<h1>Test2</h1>)
 }
 
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/t1/test" Component={Test1}/>
+      <Global styles={reset}/>
+      <MainAside />
+      <Routes>  
+        <Route path="/t1" Component={Test1}/>
         <Route path="/t2" Component={Test2}/>
         <Route path="/sample/input/1" Component={InputSample}/>
+        <Route path="/users" Component={UserList}/>
       </Routes>
     </>
   );
---
+++ react/react_study/src/components/Aside/MainAside/MainAside.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import * as S from './style';
+import { Navigation } from "react-minimal-side-navigation/lib";
+import { HiHome } from 'react-icons/hi';
+import { BsCardChecklist } from 'react-icons/bs';
+import { BiListCheck } from 'react-icons/bi';
+import { GrTest } from 'react-icons/gr';
+import { useNavigate } from 'react-router-dom';
+import { FaUsers } from 'react-icons/fa';
+/** @jsxImportSource @emotion/react */
+
+const MainAside = () => {
+    const navigate = useNavigate();
+
+    return (
+        <aside css={S.style}>
+            <Navigation
+            activeItemId="/"
+            onSelect={({itemId}) => {
+                navigate(itemId);
+            }}
+            items={[
+                {
+                    title: 'Home',
+                    itemId: '/home',
+                    elemBefore: () => <HiHome />
+                },
+                {
+                    title: 'T1',
+                    itemId: '/t1',
+                    elemBefore: () => <GrTest />
+                },
+                {
+                    title: 'T2',
+                    itemId: '/t2',
+                    elemBefore: () => <GrTest />
+                },
+                {
+                    title: 'Sample',
+                    itemId: '/sample/input/1',
+                    elemBefore: () => <BsCardChecklist />,
+                    subNav: [
+                        {
+                            title: 'input1',
+                            itemId: '/sample/input/1',
+                            elemBefore: () => <BiListCheck />
+                        }
+                    ]
+                },
+                {
+                    title: 'List',
+                    itemId: '/users',
+                    elemBefore: () => <BsCardChecklist />,
+                    subNav: [
+                        {
+                            title: '사용자 전체 조회',
+                            itemId: '/users',
+                            elemBefore: () => <FaUsers />
+                        }
+                    ]
+                },
+            ]}
+          />
+        </aside>
+    );
+};
+
+export default MainAside;
